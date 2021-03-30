b27637688e93900f49f078e213a51e7a8549047e
Ahmed El Nemer <aelnemer1@smail.ucas.edu.ps>
2021-03-30 16:56:40 +0300

    Add Base Algorithm classes

---
+++ UCASecurity.Encryption/Base/Algorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCASecurity.Encryption.Base
+{
+    public abstract class Algorithm
+    {
+        public abstract Result Encrypt(string text, string key);
+        public abstract Result Decrypt(string cipher, string key);
+        public abstract bool Health();
+    }
+}
---
+++ UCASecurity.Encryption/Base/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCASecurity.Encryption.Base
+{
+    public class Result
+    {
+        public string payload { get; set; }
+        public StatusCode status { get; set; }
+    }
+}
