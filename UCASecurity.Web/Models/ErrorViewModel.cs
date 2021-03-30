3f27ba258777f8237c47b69331694daba9f7d35b
Ahmed El Nemer <aelnemer1@smail.ucas.edu.ps>
2021-03-30 16:36:05 +0300

    Add project files.

---
+++ UCASecurity.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UCASecurity.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
