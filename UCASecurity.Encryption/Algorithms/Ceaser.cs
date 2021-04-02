b95f5419deff051fb3105afd0640ef96a22ca633
Ahmed El Nemer <aelnemer1@smail.ucas.edu.ps>
2021-04-02 13:09:01 +0300

    Add Example usage of an Algorithm

---
+++ UCASecurity.Encryption/Algorithms/Ceaser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCASecurity.Encryption.Base;
+
+namespace UCASecurity.Encryption.Algorithms
+{
+    public class Ceaser : Algorithm<string, int, string>
+    {
+        public override Result<string> Decrypt(string cipher, int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Result<string> Encrypt(string text, int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Health()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
